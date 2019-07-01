f07cc459e29c7f37167ade8aec07dfbb4da6d143
ofrost-g <romanpaumulin@gmail.com>
2019-07-01 15:45:26 +0300

    Added horizon delta calculate

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/hdwarven/Desktop/wolf3d
+# Install script for directory: /Users/ofrost-g/work/Wolf3D
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/hdwarven/Desktop/wolf3d/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ofrost-g/work/Wolf3D/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
